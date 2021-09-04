f56842de5c23938dd63cbca94ff684ec7748dc8b
Shoji Makino <pitang1965@gmail.com>
2021-09-04 16:03:16 +0900

    Add update-member.js

---
+++ api/update-member.js
@@ -0,0 +1,13 @@
+const { table } = require('./helpers/airtable');
+const formattedReturn = require('./helpers/formattedReturn');
+
+module.exports = async (event) => {
+  const { id, ...fields } = JSON.parse(event.body);
+  try {
+    const updatedMember = await table.update([{ id, fields }]);
+    return formattedReturn(200, updatedMember);
+  } catch (err) {
+    console.error(err);
+    return formattedReturn(500, {});
+  }
+};
