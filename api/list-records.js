98a3521828bbbed48f8f89094183714c6060f174
Shoji Makino <pitang1965@gmail.com>
2021-09-04 10:12:40 +0900

    Refactor and change async version list-records.js

--- api/list-records.js
+++ api/list-records.js
@@ -1,20 +1,17 @@
 const { table } = require('./helpers/airtable');
 const formattedReturn = require('./helpers/formattedReturn');
 
-exports.handler = function (event, context, callback) {
-  const reconstructedTable = [];
-
-  table
-    .select({
-      // AIRTABLE_TABLE_NAMEで指定したテーブルの最初のmaxRecords件のレコードを取得
-      maxRecords: 1500,
-      view: process.env.AIRTABLE_VIEW_NAME,
-    })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        // 本関数(`page`)はレコードのページごとに呼び出されます。
-
-        records.forEach(function (record) {
+exports.handler = async () => {
+  try {
+    const formattedMembers = [];
+    await table
+      .select({
+        // テーブルの最初のmaxRecords件のレコードを取得
+        maxRecords: 1500,
+        view: process.env.AIRTABLE_VIEW_NAME,
+      })
+      .eachPage((records, fetchNextPage) => {
+        records.forEach((record) => {
           console.log(
             'Retrieved',
             record.get('Name'),
@@ -22,18 +19,13 @@ exports.handler = function (event, context, callback) {
             record.id
           );
 
-          reconstructedTable.push({ ...record.fields, id: record.id });
+          formattedMembers.push({ ...record.fields, id: record.id });
         });
-        // 次のページを取得するには、`fetchNextPage` を呼び出してください。
-        // まだレコードがある場合、`page` が再度呼び出されます。
-        // すべてのレコードが取得された場合、`done` が呼び出されます。
         fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          callback(formattedReturn(err.statusCode, err.message));
-        }
-        callback(null, formattedReturn(200, reconstructedTable));
-      }
-    );
+      });
+    return formattedReturn(200, formattedMembers);
+  } catch (err) {
+    console.error(err);
+    return formattedReturn(err.statusCode, err.message);
+  }
 };
