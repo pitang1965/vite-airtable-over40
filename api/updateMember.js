d92713ae4be1009a2a65ec81e978ba49842aba66
Shoji Makino <pitang1965@gmail.com>
2021-09-23 01:41:01 +0900

    Update modules

--- api/updateMember.js
+++ api/updateMember.js
@@ -2,7 +2,11 @@ const { table } = require('./utils/airtable');
 const formattedReturn = require('./utils/formattedReturn');
 
 module.exports = async (event) => {
-  const { id, ...fields } = JSON.parse(event.body);
+  if (event.httpMethod !== 'PATCH') {
+    return formattedReturn(405, 'Bad request');
+  }
+
+  const { id, fields } = JSON.parse(event.body);
   try {
     const updatedMember = await table.update([{ id, fields }]);
     return formattedReturn(200, updatedMember);
