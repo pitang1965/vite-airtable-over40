16cbae1d6b7284b9e06b3fe1e21caf65708a9272
Shoji Makino <pitang1965@gmail.com>
2021-10-03 11:41:17 +0900

    Persit theme preference in local storage

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 export default () => {
   const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const localStorageTheme = localStorage.getItem('theme');
+    setTheme(localStorageTheme || 'light');
+  }, []);
 
   const toggleTheme = () => {
     if (theme === 'light') {
       setTheme('dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       setTheme('light');
+      localStorage.setItem('theme', 'light');
     }
   };
   return [theme, toggleTheme];
