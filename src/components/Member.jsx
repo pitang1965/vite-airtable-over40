7fc26a12b3d234c263030f4c2adb4c7b2eca2095
Shoji Makino <pitang1965@gmail.com>
2021-10-23 09:33:57 +0900

    Refactoring and modify CSS form member

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,65 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 import MemberEditForm from './MemberEditForm';
+import {
+  StyledMember,
+  StyledInfo,
+  StyledName,
+  ImageContainer,
+  StyledTitle,
+  StyledBio,
+} from '../styled/StyledMember';
 import { StyledButton } from '../styled/StyledButton';
-import { useAtom } from 'jotai'
+import { useAtom } from 'jotai';
 import { roleAtom } from '../atoms/auth';
-
-const StyledMember = styled.article`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  border-radius: 10px;
-  background-color: var(--card-bg-color);
-  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  width: 200px;
-  height: 300px;
-`;
-
-const StyledInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  height: 140px;
-  width: 180px;
-`;
-
-const StyledName = styled.a`
-  font-size: 1.3rem;
-  font-weight: bold;
-  letter-spacing: 1px;
-`;
-
-const ImageContainer = styled.img`
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
-`;
-
-const StyledTitle = styled.p`
-  font-size: 1rem;
-  margin-top: 0.2em;
-  margin-bottom: 0.1em;
-`;
-
-const StyledBio = styled.div`
-  flex-grow: 1;
-  margin: 0px;
-  overflow: auto;
-  background-color: var(--card-textarer-bg-color);
-  border-radius: 10px;
-  font-size: 0.8em;
-  text-align: left;
-  padding: 10px;
-  height: 50px;
-  width: 100%;
-`;
 
 const Member = ({ id, fields: prevFields, updateMember }) => {
   const [visible, setVisible] = useState(false);
---
+++ src/styled/StyledMember.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const StyledMember = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+  border-radius: 10px;
+  background-color: var(--card-bg-color);
+  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
+  margin: 10px;
+  padding: 10px;
+  text-align: center;
+  width: 300px;
+  height: 400px;
+`;
+
+export const StyledInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-grow: 1;
+  width: 90%;
+`;
+
+export const StyledName = styled.a`
+  font-size: 1.3rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+export const ImageContainer = styled.img`
+  border-radius: 50%;
+  width: 180px;
+  height: 180px;
+`;
+
+export const StyledTitle = styled.p`
+  font-size:1rem;
+  margin-top: 0.2em;
+  margin-bottom: 0.1em;
+`;
+
+export const StyledBio = styled.div`
+  flex-grow: 1;
+  margin: 0px;
+  overflow: auto;
+  background-color: var(--card-textarer-bg-color);
+  border-radius: 10px;
+  font-size: 1rem;
+  text-align: left;
+  padding: 10px;
+  height: 80px;
+  width: 100%;
+`;
