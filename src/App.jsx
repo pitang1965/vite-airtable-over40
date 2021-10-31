28ba8efbb0674a6f8c1d96727618fe2c8cc047e0
Shoji Makino <pitang1965@gmail.com>
2021-10-31 16:09:10 +0900

    Add notification that Safari cannot be used

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FooterMenu from './components/FooterMenu';
 import styled, { ThemeProvider } from 'styled-components';
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Workshop from './pages/Workshop';
 import About from './pages/About';
 import Navber from './components/Navbar';
+import toast, { Toaster } from 'react-hot-toast';
 
 const StyledApp = styled.div`
   min-height: 100vh;
@@ -27,12 +28,24 @@ const StyledApp = styled.div`
 function App() {
   const [theme, toggleTheme] = useTheme();
   const currentTheme = theme === 'light' ? lightTheme : darkTheme;
+  useEffect(() => {
+    const ua = window.navigator.userAgent.toLowerCase();
+    if (ua.indexOf('safari') > 1 && ua.indexOf('chrome') === -1) {
+      toast.error(
+        'Safariはサポートされていません。ChromaかFirefoxを使用してください。',
+        {
+          icon: '😇',
+        }
+      );
+    }
+  }, []);
 
   return (
     <Router>
       <ThemeProvider theme={currentTheme}>
         <GlobalStyle />
         <StyledApp>
+          <Toaster />
           <Navber toggleTheme={toggleTheme} />
           <Switch>
             <Route path='/about' component={About} />
