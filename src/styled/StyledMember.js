f55c213e8017aee5bc644097acc6c84f1c2874b9
takunagai <tak_na@icloud.com>
2021-11-06 12:09:59 +0900

    見た目の調整(余白、文字サイズ、SNSボタン色)、CSSの最適化

--- src/styled/StyledMember.js
+++ src/styled/StyledMember.js
@@ -1,62 +1,62 @@
 import styled from 'styled-components';
 
 export const StyledMember = styled.article`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  gap: 5px;
-  border-radius: 10px;
-  background-color: var(--card-bg-color);
-  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
   width: 300px;
-  height: 400px;
+  margin: 10px;
+  @media only screen and (min-width:1200px) {
+	margin: 20px;
+  }
+  padding: 30px 20px 20px;
+  border-radius: 7px;
+  background-color: var(--card-bg-color);
+  box-shadow: 0 3px 15px rgba(100, 100, 100, .2);
+  text-align: center;
 `;
 
 export const StyledInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  flex-grow: 1;
-  width: 90%;
+  margin-top: 10px;
 `;
 
 export const StyledName = styled.p`
-  font-size: 1.3rem;
+  margin-bottom: .1em;
+  font-size: 1.3em;
   font-weight: bold;
-  letter-spacing: 1px;
+  letter-spacing: .1em;
 `;
 
 export const ImageContainer = styled.img`
   border-radius: 50%;
-  width: 140px;
-  height: 140px;
+  width: 150px;
+  height: 150px;
 `;
 
 export const StyledTitle = styled.p`
-  font-size: 1rem;
-  margin-top: 0.2em;
-  margin-bottom: 0.1em;
+  font-size: .9em;
 `;
 
 export const StyledBio = styled.div`
-  flex-grow: 1;
-  margin: 0px;
+  height: 5em;
+  padding:7px;
   overflow: auto;
   background-color: var(--card-textarer-bg-color);
-  border-radius: 10px;
-  font-size: 1rem;
   text-align: left;
-  padding: 10px;
-  height: 80px;
-  width: 100%;
 `;
 
 export const StyledSnsIcons = styled.div`
   display: flex;
-  margin-top: 5px;
+  justify-content: center;
+  margin-top: 1em;
+  .circle-icon {
+    background-color: #E7E7E7;
+    svg {
+      fill: #737373;
+      margin-top: .5em;
+    }
+    &:hover {
+      background-color: #efefbb;
+      svg {
+        fill: #a4a440;
+      }
+    }
+  }
 `;
