95e768b7344747c9fa14e63bffd83145f4885eb2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-11-17 19:04:42 +0900

    ⚰️ remove dead code

--- src/pages/WorkshopPage.jsx
+++ src/pages/WorkshopPage.jsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { StyledPageTitle } from '../styled/StyledPageTitle';
-import Workshops from '../components/Workshops';
-
-// const StyledWorkshop = styled.div`
-//   height: 80vh;
-//   width: 70vw;
-//   max-width: 800px;
-//   display: grid;
-//   grid-template-rows: 50px 1fr;
-//   font-size: 1.5rem;
-//   gap: 2rem;
-// `;
+import React from "react";
+import { StyledPageTitle } from "../styled/StyledPageTitle";
+import Workshops from "../components/Workshops";
 
 const WorkshopPage = () => {
   return (
@@ -19,7 +8,7 @@ const WorkshopPage = () => {
       <StyledPageTitle>勉強会について</StyledPageTitle>
       <main>
         <p>これまで開催した勉強会資料は以下の通り。</p>
-        <Workshops/>
+        <Workshops />
       </main>
     </div>
   );
