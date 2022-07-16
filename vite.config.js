c85a4c1ad51527b826401d57fe91021b8856d032
Shoji Makino <pitang1965@gmail.com>
2022-07-16 21:50:47 +0900

    Fix: type of port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: '3000',
+    port: 3000,
   },
   plugins: [reactRefresh()],
 });
